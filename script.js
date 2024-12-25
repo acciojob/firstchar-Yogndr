fa3a2f4b3ee3a56a8af66e982b405dbb3a59425d
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-25 23:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
+	
+	let ans=text.trim();
+	if(ans.length==0){
+		return "";
+	}
+	return ans[0];
   // your code here
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// const text = prompt("Enter text:");
 alert(firstChar(text));
